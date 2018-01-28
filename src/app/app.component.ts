a19abb1abff3a1c963f8fc0428a5b3aea47fe288
Simona Cotin <simona.cotin@gmail.com>
2018-01-28 10:03:02 +0000

    Add store, reducers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { State } from './reducers/index';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  counter$: Observable<number>;
+
+  constructor(private store: Store<State>) {
+    this.counter$ = store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch({ type: 'INCREMENT' });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: 'DECREMENT' });
+  }
 }
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,14 +6,23 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { Action } from '@ngrx/store/src/models';
 
 export interface State {
-
+  counter: number;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const reducers = (state: number = 0, action: Action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
 };
 
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? []
+  : [];
